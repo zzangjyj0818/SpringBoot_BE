70dffe0ad52a2a52c3b44aec129d5ea5f8e1b85f
zzangjyj0818 <jyj010818@naver.com>
2023-10-31 15:13:04 +0900

    Implement the Spring Security(1/4)

---
+++ cardatabase/src/main/java/com/example/cardatabase/domain/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.cardatabase.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
