212edcd5c43281ef03b8adadd90b84821311fd95
zzangjyj0818 <jyj010818@naver.com>
2023-11-01 14:24:12 +0900

    new Project

---
+++ pratice/src/test/java/com/example/pratice/PraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
