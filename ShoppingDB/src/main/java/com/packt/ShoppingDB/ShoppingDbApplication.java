8a0c938fa1c42af7761ba59871b50059827a0618
zzangjyj0818 <jyj010818@naver.com>
2023-12-20 22:11:22 +0900

    commit

---
+++ ShoppingDB/src/main/java/com/packt/ShoppingDB/ShoppingDbApplication.java
@@ -0,0 +1,13 @@
+package com.packt.ShoppingDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingDbApplication.class, args);
+	}
+
+}
---
+++ ShoppingDB/src/test/java/com/packt/ShoppingDB/ShoppingDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packt.ShoppingDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
