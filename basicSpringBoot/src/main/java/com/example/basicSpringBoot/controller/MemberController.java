9ac5fb6c541b6e1a758d93933a5527b70aba3971
zzangjyj0818 <jyj010818@naver.com>
2024-02-03 23:49:25 +0900

    CRUD Practice

--- basicSpringBoot/src/main/java/com/example/basicSpringBoot/controller/MemberController.java
+++ basicSpringBoot/src/main/java/com/example/basicSpringBoot/controller/MemberController.java
@@ -5,8 +5,12 @@ import com.example.basicSpringBoot.entity.Member;
 import com.example.basicSpringBoot.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -16,6 +20,20 @@ public class MemberController {
     @GetMapping("/signup")
     public String singUp(){
         return "/members/new";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model){
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", memberEntity);
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model){
+        List<Member> memberEntityList = memberRepository.findAll();
+        model.addAttribute("memberList", memberEntityList);
+        return "/members/index";
     }
 
     @PostMapping("/join")
--- basicSpringBoot/src/main/java/com/example/basicSpringBoot/repository/MemberRepository.java
+++ basicSpringBoot/src/main/java/com/example/basicSpringBoot/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.example.basicSpringBoot.repository;
 import com.example.basicSpringBoot.entity.Member;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface MemberRepository extends CrudRepository<Member, Long> {
+    @Override
+    ArrayList<Member> findAll();
 }
