9da0a8d7621879928dfa522fec832864490538d5
zzangjyj0818 <jyj010818@naver.com>
2024-02-03 23:39:37 +0900

    Read

--- basicSpringBoot/src/main/java/com/example/basicSpringBoot/controller/ArticleController.java
+++ basicSpringBoot/src/main/java/com/example/basicSpringBoot/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.basicSpringBoot.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -17,6 +21,20 @@ public class ArticleController {
     @GetMapping("/articles/new")
     public String newArticleForm(){
         return "/articles/new";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String createArticle(@PathVariable Long id, Model model){
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article", articleEntity);
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model){
+        List<Article> articleEntityList = articleRepository.findAll();
+        model.addAttribute("articleList", articleEntityList);
+        return "articles/index";
     }
 
     @PostMapping("/articles/create")
--- basicSpringBoot/src/main/java/com/example/basicSpringBoot/repository/ArticleRepository.java
+++ basicSpringBoot/src/main/java/com/example/basicSpringBoot/repository/ArticleRepository.java
@@ -3,5 +3,9 @@ package com.example.basicSpringBoot.repository;
 import com.example.basicSpringBoot.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
