0f05157c0db17584c3831442d3f8a4dd7ebee446
zzangjyj0818 <jyj010818@naver.com>
2024-02-09 00:28:55 +0900

    rest api[post]

--- basicSpringBoot/src/main/java/com/example/basicSpringBoot/api/CommentApiController.java
+++ basicSpringBoot/src/main/java/com/example/basicSpringBoot/api/CommentApiController.java
@@ -1,11 +1,28 @@
 package com.example.basicSpringBoot.api;
 
+import com.example.basicSpringBoot.dto.CommentDto;
 import com.example.basicSpringBoot.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CommentApiController {
     @Autowired
     private CommentService commentService;
+
+    // CREATE
+
+    // READ
+    @GetMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId){
+        List<CommentDto> dto = commentService.comments(articleId);
+        return ResponseEntity.ok(dto);
+    }
+    // UPDATE
+    // DELETE
 }
--- basicSpringBoot/src/main/java/com/example/basicSpringBoot/service/CommentService.java
+++ basicSpringBoot/src/main/java/com/example/basicSpringBoot/service/CommentService.java
@@ -1,9 +1,15 @@
 package com.example.basicSpringBoot.service;
 
+import com.example.basicSpringBoot.dto.CommentDto;
+import com.example.basicSpringBoot.entity.Comment;
 import com.example.basicSpringBoot.repository.ArticleRepository;
 import com.example.basicSpringBoot.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -11,4 +17,11 @@ public class CommentService {
     private CommentRepository commentRepository;
     @Autowired
     private ArticleRepository articleRepository;
+
+    public List<CommentDto> comments(Long articleId) {
+        return commentRepository.findByArticleId(articleId)
+                .stream()
+                .map(comment -> CommentDto.createCommentDto(comment))
+                .collect(Collectors.toList());
+    }
 }
